88134fa427a84b7eb555767c125aa0815a2a6499
Alexander Bolotskov <r3disrussian@gmail.com>
2021-04-22 02:29:04 +0800

    fix: 🐛 removes IInstanceOptions interface
    It's no use anymore

--- src/interface.ts
+++ src/interface.ts
@@ -1,8 +1,1 @@
-import { LogLevel } from 'consola'
-
 export { LogLevel } from 'consola'
-
-export interface IInstanceOptions {
-  /** Logging level. Default is 1 (includes errors and warns) */
-  logLevel?: LogLevel
-}
